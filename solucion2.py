b6d5b5daf86fcbd1dd7754930ee0330305441449
Belkis <katerinne65@gmail.com>
2022-04-15 18:45:13 +0200

    Metodo leer archivo y separar la cadena

--- solucion2.py
+++ solucion2.py
@@ -1,6 +1,32 @@
 from io import open
+
+
 resp = "0"
 lista = []
+
+def leer_archivo(): # Metodo para leer el archivo 
+    try:
+        fichero = open('input.txt','r')
+        lineas = fichero.readlines()
+        fichero.close()
+        for linea in lineas:
+            lista.append(separador_cadena(linea))
+    except FileNotFoundError:
+        print("No se ha conseguido el archivo")        
+    except:
+        print("Ha ocurrido un error con el archivo")
+    finally:
+        print("Ha finalizado el programa")
+        quit()
+    
+    
+def separador_cadena(linea): # Metodo que procesa la linea de entrada	
+	linea1=linea.replace(':' , '')
+	linea2=linea1.replace('-' , ' ')
+	cadenaespacios=linea2.split(' ')	
+	return cadenaespacios
+
+leer_archivo()
 
 while (resp!= '3'):	
 	print("\n**  Menú  ***")
